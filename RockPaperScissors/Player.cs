b87431cc6e4c8f62b8df5a13708b855f06f08157
Garrett <davisgarrett21@yahoo.com>
2018-05-09 20:58:36 -0500

    Added a list for choices that can result from the AI or Human Class

--- RockPaperScissors/Player.cs
+++ RockPaperScissors/Player.cs
@@ -6,17 +6,30 @@ using System.Threading.Tasks;
 
 namespace RockPaperScissors
 {
-    public class Player { 
-        
-        
-    
+    public class Player
+    {
+
+
+
         public void ChooseRockPaperScissorsLizardOrSpock()
         {
             Console.Write("Choose Rock, Paper, Scissors, Lizard, or Spock.");
             Console.ReadLine();
 
-        
+
         }
+
+        public void Choices()
+        {
+            List<string> choices = new List<string>();
+            choices.Add("Rock");
+            choices.Add("Paper");
+            choices.Add("Scissors");
+            choices.Add("Lizard");
+            choices.Add("Spock");
         }
+
+
     }
+}
 
