bfabd4490c51117ef90a51dd279db62c1919fa4c
Garrett <davisgarrett21@yahoo.com>
2018-05-09 19:43:55 -0500

    Changes made to the AI Method, and added a random integer function that displayed either Rock, Paper, Scissors, Lizard, or Spock

--- RockPaperScissors/AI.cs
+++ RockPaperScissors/AI.cs
@@ -17,8 +17,40 @@ namespace RockPaperScissors
         {
             Random rnd = new Random();
             int choice = rnd.Next(1, 5);
-            Console.Write(choice);
-            Console.ReadLine();
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Rock");
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    Console.Write("Paper");
+                    Console.ReadLine();
+                    break;
+
+                case 3:
+                    Console.Write("Scissors");
+                    Console.ReadLine();
+                    break;
+
+                case 4:
+                    Console.Write("Lizard");
+                    Console.ReadLine();
+                    break;
+
+                case 5:
+                    Console.Write("Spock");
+                    Console.ReadLine();
+                    break;
+
+                default:
+                    break;
+                
+                
+            }
+            
         }
     }
 }
